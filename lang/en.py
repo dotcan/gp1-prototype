8d804b56c2e35fca5386dec561555d43982153f9
dotalw <15912596+dotalw@users.noreply.github.com>
2024-01-01 23:51:45 +0400

    Created lang/en

---
+++ lang/en.py
@@ -0,0 +1,205 @@
+ttb = {
+    'langauge': 'English',
+    'letter_conversion': [
+        {
+            'letter': 'a',
+            'display': 'A',
+            'braille': u'\u2801',
+        },
+        {
+            'letter': 'b',
+            'display': 'B',
+            'braille': u'\u2803',
+        },
+        {
+            'letter': 'c',
+            'display': 'C',
+            'braille': u'\u2809',
+        },
+        {
+            'letter': 'd',
+            'display': 'D',
+            'braille': u'\u2819',
+        },
+        {
+            'letter': 'e',
+            'display': 'E',
+            'braille': u'\u2811',
+        },
+        {
+            'letter': 'f',
+            'display': 'F',
+            'braille': u'\u280b',
+        },
+        {
+            'letter': 'g',
+            'display': 'G',
+            'braille': u'\u281b',
+        },
+        {
+            'letter': 'h',
+            'display': 'H',
+            'braille': u'\u2813',
+        },
+        {
+            'letter': 'i',
+            'display': 'I',
+            'braille': u'\u280a',
+        },
+        {
+            'letter': 'j',
+            'display': 'J',
+            'braille': u'\u281a',
+        },
+        {
+            'letter': 'k',
+            'display': 'K',
+            'braille': u'\u2805',
+        },
+        {
+            'letter': 'l',
+            'display': 'L',
+            'braille': u'\u2807',
+        },
+        {
+            'letter': 'm',
+            'display': 'M',
+            'braille': u'\u280d',
+        },
+        {
+            'letter': 'n',
+            'display': 'N',
+            'braille': u'\u281d',
+        },
+        {
+            'letter': 'o',
+            'display': 'O',
+            'braille': u'\u2815',
+        },
+        {
+            'letter': 'p',
+            'display': 'P',
+            'braille': u'\u280f',
+        },
+        {
+            'letter': 'q',
+            'display': 'Q',
+            'braille': u'\u281f',
+        },
+        {
+            'letter': 'r',
+            'display': 'R',
+            'braille': u'\u2817',
+        },
+        {
+            'letter': 's',
+            'display': 'S',
+            'braille': u'\u280e',
+        },
+        {
+            'letter': 't',
+            'display': 'T',
+            'braille': u'\u281e',
+        },
+        {
+            'letter': 'u',
+            'display': 'U',
+            'braille': u'\u2825',
+        },
+        {
+            'letter': 'v',
+            'display': 'V',
+            'braille': u'\u2827',
+        },
+        {
+            'letter': 'w',
+            'display': 'W',
+            'braille': u'\u283a',
+        },
+        {
+            'letter': 'x',
+            'display': 'X',
+            'braille': u'\u282d',
+        },
+        {
+            'letter': 'y',
+            'display': 'Y',
+            'braille': u'\u283d',
+        },
+        {
+            'letter': 'z',
+            'display': 'Z',
+            'braille': u'\u2835',
+        },
+    ],
+    'number_conversion': [
+        {
+            'num': 1,
+            'display': '1',
+            'braille': u'\u2802',
+        },
+        {
+            'num': 2,
+            'display': '2',
+            'braille': u'\u2806',
+        },
+        {
+            'num': 3,
+            'display': '3',
+            'braille': u'\u2812',
+        },
+        {
+            'num': 4,
+            'display': '4',
+            'braille': u'\u2832',
+        },
+        {
+            'num': 5,
+            'display': '5',
+            'braille': u'\u2822',
+        },
+        {
+            'num': 6,
+            'display': '6',
+            'braille': u'\u2816',
+        },
+        {
+            'num': 7,
+            'display': '7',
+            'braille': u'\u2836',
+        },
+        {
+            'num': 8,
+            'display': '8',
+            'braille': u'\u2826',
+        },
+        {
+            'num': 9,
+            'display': '9',
+            'braille': u'\u2814',
+        },
+        {
+            'num': 0,
+            'display': '0',
+            'braille': u'\u2834',
+        },
+    ],
+    'special_conversion': [
+        {
+            'letter': '',
+            'display': 'number sign',
+            'braille': '\u283c',
+            'hidden': True,
+        },
+        {
+            'letter': ' ',
+            'display': 'SPACE',
+            'braille': u'\u2800'
+        },
+        {
+            'letter': '\n',
+            'display': 'ENTER',
+            'braille': '\n',
+        },
+    ]
+}
