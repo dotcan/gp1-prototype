0d705756469424802795139f4f80a34c2f251885
dotalw <15912596+dotalw@users.noreply.github.com>
2024-01-02 22:32:45 +0400

    Added experimental auto tts Removed voice selector

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_javascript import st_javascript as st_js
 
 import tts
 from lang.en import ttb
-from utils import gen_cols, mobile_markdown, state_append_braille, state_append_num, state_pop
+from utils import gen_cols, mobile_markdown, state_append_braille, state_append_num
 
 st.set_page_config(page_title="DotBraille GP1 Demo")
 
@@ -17,20 +17,24 @@ if 'text' not in st.session_state:
     st.session_state.text = ''
 if 'braille' not in st.session_state:
     st.session_state.braille = ''
-if 'speech' not in st.session_state:
-    st.session_state.speech = 'male'
 if 'cols' not in st.session_state:
     st.session_state.cols = 1
 
 with st.sidebar:
-
     st.header("DotBraille GP1 Demo")
     with st.expander("About this app"):
         st.markdown("This is a proof of concept demo that demonstrates how braille to text conversion works, "
                     "and converting text to speech")
     with st.expander("Configuration", expanded=True):
-        disp_letters = st.checkbox("Display Letters", value=True)
-        st.session_state.speech = st.selectbox("Voice", options=['male', 'female'])
+        disp_chars = st.checkbox(
+            label="Display Characters",
+            value=True,
+            help="Show/Hide English letters/numbers next to braille symbols",
+        )
+        auto_convert = st.checkbox(
+            label="Auto Convert TTS (experimental)",
+            value=False,
+        )
     if args.dev:
         with st.expander('dev'):
             st.json(st.session_state)
@@ -46,16 +50,22 @@ st.code(st.session_state.braille)
 st.write("##### Converted Text")
 st.code(st.session_state.text)
 
-col1, col2 = st.columns(2)
-if col1.button('convert to speech', disabled=(st.session_state.text == '')):
-    col2.audio(tts.convert(st.session_state.text), format='audio/mpeg')
+if not auto_convert:
+    col1, col2 = st.columns(2)
+    if col1.button('convert to speech', disabled=(st.session_state.text == '')):
+        col2.audio(tts.convert(st.session_state.text), format='audio/mpeg')
+else:
+    if st.session_state.text:
+        with st.container(border=True):
+            st.write("###### Converted Speech")
+            st.audio(tts.convert(st.session_state.text), format='audio/mpeg')
 
 st.write("##### Input")
 with st.expander(label="Letters", expanded=True):
     with gen_cols(ttb['letter_conversion'], st.session_state.cols) as column:
         for i, di in enumerate(ttb['letter_conversion']):
             column[i].button(
-                label=f"{di['braille']}{di['display'] if disp_letters else ''}",
+                label=f"{di['braille']}{di['display'] if disp_chars else ''}",
                 help=di['display'],
                 on_click=state_append_braille,
                 args=[di['letter'], di['braille']],
@@ -65,7 +75,7 @@ with st.expander(label="Numbers", expanded=True):
     with gen_cols(ttb['number_conversion'], st.session_state.cols) as column:
         for i, di in enumerate(ttb['number_conversion']):
             column[i].button(
-                label=f"{di['braille']}{di['display'] if disp_letters else ''}",
+                label=f"{di['braille']}{di['display'] if disp_chars else ''}",
                 help=di['display'],
                 on_click=state_append_num,
                 args=[di['num'], di['braille'], ttb],
